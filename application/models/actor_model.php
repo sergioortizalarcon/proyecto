188b39be164f16529714206f1e0f875611c94e21
alumno <alumno@TALLER3-3>
2018-03-20 08:40:26 +0100

    m

---
+++ application/models/actor_model.php
@@ -0,0 +1,25 @@
+<?php
+class Actor_model extends CI_Model {
+	public function create_actor($nombre_actor, $apellido1_actor, $apellido2_actor, $fechaNac_actor, $nacionalidad_actor) {
+		$actor = R::findOne('actor','nombre=?',[$nombre_actor]);
+		if ($actor == null)  {
+			$a = R::dispense ( 'actor' );
+			$a -> nombre = $nombre_actor;
+			$a -> apellido1 = $apellido1_actor;
+			$a -> apellido2 = $apellido2_actor;
+			$a -> fechaNac = $fechaNac_actor;
+			$a -> nacionalidad = $nacionalidad_actor;
+			R::store($a);
+		}
+		else {
+			throw new Exception("Actor duplicada");
+		}
+		R::close();
+	}
+
+	public function getAll($filtro='') {
+		$mostrar = R::find("actor","nombre like ?", ["%".$filtro."%"]);
+		return $mostrar;
+	}
+}
+?>

34d05c72ffa93ee297a762d6ff12dc372ebc7f3f
Miguel Abalo <becariodevelopment@tunstall.es>
2018-06-05 17:54:23 +0200

    a

--- application/views/actor/ficha.php
+++ application/views/actor/ficha.php
@@ -6,6 +6,7 @@
 				<li><a href="#bio"> <i class="fas fa-book"></i> Biografía </a></li>
 				<li><a href="#filmog"> <i class="fas fa-film"></i> Filmografía </a></li>
 				<li><a href="#noticias"> <i class="far fa-newspaper"></i> Noticia </a></li>
+				<li><a href="#galeria"> <i class="far fa-images"></i> Galería </a></li>
 			</ul>
 		
 			<div id="perfil">
@@ -17,14 +18,17 @@
 						<h4><?= $body['actores']->nombre ?> <?= $body['actores']->apellido1 ?> <?= $body['actores']->apellido2 ?></h4>
 					</div>
 				</div>
-				<h5><?= $body['actores']->cadProfesiones ?></h5>
-				<?php if($body['actores']->ambos == 'on'): ?>
-					<h5>Actor / Director</h5>
-				<?php else: ?>
-			    	<h5>Actor</h5>
-				<?php endif; ?>
-				<h5>Fecha de nacimiento: <?=$body['actores']->fechaNacimiento ?></h5>
-				<h5>País de nacimiento: <?= $body['actores']->paises['nombre'] ?></h5>
+				<div class="row-md-12" style="height: 100%;margin: 1% 1% 5% 0;">
+					<h5><?= $body['actores']->cadProfesiones ?></h5>
+    				<!-- TEMPORAL depende de lo que decidamos quitar el de arriba o el de abajo -->
+    				<?php if($body['actores']->ambos == 'on'): ?>
+    					<h5>Actor / Director</h5>
+    				<?php else: ?>
+    			    	<h5>Actor</h5>
+    				<?php endif; ?>
+    				<h5>Fecha de nacimiento: <?=$body['actores']->fechaNacimiento ?></h5>
+    				<h5>País de nacimiento: <?= $body['actores']->paises['nombre'] ?></h5>
+				</div>
 			</div>
 			
 			<div id="bio">
@@ -36,7 +40,9 @@
 						<h4><?= $body['actores']->nombre ?> <?= $body['actores']->apellido1 ?> <?= $body['actores']->apellido2 ?></h4>
 					</div>
 				</div>
-				<?= $body['actores']->biografia ?>
+				<div class="row-md-12" style="height: 100%;margin: 1% 1% 5% 0;">
+					<?= $body['actores']->biografia ?>
+				</div>
 			</div>
 			
 			<div id="filmog">
@@ -48,7 +54,9 @@
 						<h4><?= $body['actores']->nombre ?> <?= $body['actores']->apellido1 ?> <?= $body['actores']->apellido2 ?></h4>
 					</div>
 				</div>
-				Lorem ipsum dolor sit amet.Lorem ipsum dolor.
+				<div class="row-md-12" style="height: 100%;margin: 1% 1% 5% 0;">
+					Lorem ipsum dolor sit amet.Lorem ipsum dolor.
+				</div>
 			</div>
 			
 			<div id="noticias">
@@ -56,13 +64,52 @@
 					<div class="col-md-12">
 						<img src="<?= base_url() ?><?= $body['actores']->rutaFoto ?>">
 					</div>
-					<div class="col-md-12"">
+					<div class="col-md-12">
 						<h4><?= $body['actores']->nombre ?> <?= $body['actores']->apellido1 ?> <?= $body['actores']->apellido2 ?></h4>
 					</div>
 				</div>
-				Lorem ipsum.
+				<div class="row-md-12" style="height: 100%;margin: 1% 1% 5% 0;">
+					Lorem ipsum.
+				</div>
 			</div>
-
+			
+			<div id="galeria">
+				<div class="col-md-2">
+    				<div class="rowd" style="float: left;display: inline-grid;height: 100%;margin: 1% 1% 5% 0;border:1px solid red;">
+    					<div class="col-md-12">
+    						<img src="<?= base_url() ?><?= $body['actores']->rutaFoto ?>">
+    					</div>
+    					<div class="col-md-12">
+    						<h4><?= $body['actores']->nombre ?> <?= $body['actores']->apellido1 ?> <?= $body['actores']->apellido2 ?></h4>
+    					</div>
+    					<!-- Cuando se pase el rol del usuario, si es admin, puede ver el botón, si no, solo las fotos -->
+    					<?php //if($body['usuarios']->rol == 3): ?>
+    					<div class="col-md-12" style="height: 100%;margin: 1% 1% 5% 0;">
+    						<input class="btn btn-default" type="button" onclick="insertarFotos();" value="Añadir fotos" />
+    					</div>
+    					<?php //endif; ?>
+    				</div>
+				</div>
+				<!-- TEMPORAL añadir imagenes a la galería de cada persona -->
+    			<div class="col-md-10">
+    				<form action="<?= base_url()?>actor/insertarImagenes" method="post" enctype="multipart/form-data">
+    					<div class="rowd" id="galeriaFotografica" style="height:100%; width:100%; border:1px solid black;"></div>
+    				</form>
+				</div>
+			</div>
 		</div>
 	</section>
 </div>
+
+<script type="text/javascript">
+	function insertarFotos() {
+		//Abrir un popup que permita insertar una foto, que la valide, si es correcta la pone mediante
+		//AJAX en la pestaña de galería, si es incorrecta, muestra un mensaje y te permite poner otra,
+		//hasta que no se pulse n el botón cerrar, no se cierra el popup
+		/*alert("insertarFotos");
+		document.getElementById("galeriaFotografica").innerHTML += "Prueba";*/
+		var opciones = "width=120,height=300,scrollbars=NO";
+
+		window.open("","nombreventa na", opciones);
+	}
+</script>

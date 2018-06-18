91e7cf5e5125ac510e5fb05790fcb796b68e2329
carlos <carlos@Equipo>
2018-06-19 00:01:40 +0200

    a

--- application/views/pelicula/ficha.php
+++ application/views/pelicula/ficha.php
@@ -13,7 +13,7 @@
 				<li><a href="#principal"><i class="far fa-id-card"></i></a></li>
 				<li><a href="#sinopsis"> <i class="fas fa-book"></i> Sinopsis </a></li>
 				<li><a href="#reparto"> <i class="fas fa-film"></i> Reparto </a></li>
-				<li><a href="#galeria"> <i class="far fa-images"></i> Galería </a></li>
+				<!--<li><a href="#galeria"> <i class="far fa-images"></i> Galería </a></li>-->
 			</ul>
 			<div id="principal">
 				<div class="col-md-3" style="padding-top:20px;">
@@ -98,6 +98,7 @@
 				</div>
 			</div>
 			
+			<!-- GALERÍa
 			<div id="galeria">
 				<div class="col-md-3" style="padding-top:20px;">
     				<div class="rowd" style="float: left;display: inline-grid;height: 100%;margin: 1% 1% 5% 0;">
@@ -107,7 +108,6 @@
     					<div class="col-md-12">
     						<h4><?= $body['peliculas']->titulo ?></h4>
     					</div>
-    					<!-- Cuando se pase el rol del usuario, si es admin, puede ver el botón, si no, solo las fotos -->
     					<?php //if($body['usuarios']->rol == 3): ?>
     					<div class="col-md-12" style="height: 100%;margin: 1% 1% 5% 0;">
     						<input class="btn btn-default" type="button" onclick="insertarFotos();" value="Añadir fotos" />
@@ -115,13 +115,12 @@
     					<?php //endif; ?>
     				</div>
 				</div>
-				<!-- TEMPORAL añadir imagenes a la galería de cada persona -->
     			<div class="col-md-9" style="padding-top:20px;">
     				<form action="<?= base_url()?>reparto/insertarImagenes" method="post" enctype="multipart/form-data">
     					<div class="rowd" id="galeriaFotografica"></div>
     				</form>
 				</div>
-			</div>
+			</div>-->
 		</div>
 	</section>
 </div>
@@ -136,108 +135,121 @@
 <?php else: ?>
 <div class="content-wrapper">
     <section class="content">
-        <div id="tabs" class="divPestanas">
-            <ul>
-                <li><a href="#principal"><i class="far fa-id-card"></i></a></li>
-                <li><a href="#sinopsis"> <i class="fas fa-book"></i> Sinopsis </a></li>
-                <li><a href="#reparto"> <i class="fas fa-film"></i> Reparto </a></li>
-                <li><a href="#galeria"> <i class="far fa-images"></i> Galería </a></li>
-            </ul>
-            <div id="principal">
-                <div class="estilo-img posicion-div-info">
-                    <img src="<?= $body['peliculas']->ruta_cartel ?>" class="imgPerfilFichaIndividual" />
-                    <h4><?= $body['peliculas']->titulo ?></h4>
-                    <?php if(isset($_SESSION['idUser'])):?>
-                    <div class="info-gen aviso-user" title="Has calificado con <?=isset($body['votos']['voto'])?$body['votos']['voto']:'0'?>/5 esta película">
-                        <input type="hidden" id="userId" value="<?=$_SESSION['idUser']?>" name="user"/>
-                        <input name="rating" value="<?=isset($body['votos']['voto'])?$body['votos']['voto']:' '?>" id="rating_star" type="hidden" postID="<?=$body['peliculas']['id']?>" />
-                    </div>
-                    <?php else: ?>
-                        <a class="info-gen" href="<?=base_url()?>Login/loginGet" title="Debes estar logueado para poder votar esta película">
-                        <input type="hidden" id="userId" value="0" name="user"/>
-                        <input name="rating" value="<?=$body['peliculas']['media_votos_totales']?>" id="rating_star" type="hidden" postID="<?=$body['peliculas']['id']?>" />
-                        </a>
-                    <?php endif; ?>
-                    <div class="overall-rating">
-                        <h6>(Calificación media <span id="avgrat"><?php echo $body['peliculas']['media_votos_totales']; ?>
-                        </span>
-                        <br/>basada en <span id="totalrat"><?php echo $body['peliculas']['votos_totales']; ?></span> votos totales)</h6>
-                    </div>
-                </div>
-                <div class="posicion-div-info">
-                    <h5>Título original: <?=$body['peliculas']->titulo_original ?></h5>
-                    <h5>Fecha de estreno: <?=$body['peliculas']->fecha_lanzamiento ?></h5>
-                    Géneros: 
-                    <?php foreach ($body['peliculas']->sharedGenerosList as $gen): ?>
-                         <?= $gen->nombre ?>
-                    <?php endforeach; ?>
-                    <h5>¿Todos los públicos? <?= $body['peliculas']->adulto ?></h5>
-                    <h5>Idioma original: <?= $body['peliculas']->original_language ?></h5>
-                </div>
-            </div>
-            
-            <div id="sinopsis">
-                <div class="estilo-img posicion-div-info">
-                    <img src="<?= $body['peliculas']->ruta_cartel ?>" class="imgPerfilFichaIndividual" />
-                    <br/>
-                    <h4><?= $body['peliculas']->titulo ?></h4>
-                </div>
-                <div class="posicion-div-info" style="width: 70%;margin-top: 2%;">
-                    <!-- <hr/> -->
-    
-                    <?= $body['peliculas']->sinopsis ?>
-                </div>
-            </div>
-            
-            <div id="reparto">
-                <div class="estilo-img posicion-div-info">
-                    <img src="<?= $body['peliculas']->ruta_cartel ?>" class="imgPerfilFichaIndividual" />
-                <br/>
-                     <h4><?= $body['peliculas']->titulo ?></h4>
-                </div>
-                <div class="posicion-div-info">
-                    <div>
-                        <div>
-	                        <h4>Reparto de la película:</h4>
-	                        <?php foreach($body['peliculas']->sharedRepartosList as $dir): ?>
-	                        <div id="<?= $dir->id ?> onclick="mostrarFicha(this.id);">
-		                        <img src="<?= $dir->ruta_foto ?>" class="imgPerfilFichaIndividual" />
-		                        <?= $dir->nombre ?> <?= $dir->apellido1 ?> <?= $dir->apellido2 ?>
-		                            <?php echo "<br>" ?>
-		                        <?php endforeach; ?>
-	                        </div>
-                        </div>
-
-                    <!-- <h4>Actores:</h4>
-                        <?php foreach($body['peliculas']->sharedActoresList as $reparto): ?>
-                            <img src="<?= $reparto->ruta_foto ?>" class="imgPerfilFichaIndividual" />
-                                <?= $reparto->nombre ?> <?= $reparto->apellido1 ?> <?= $reparto->apellido2 ?>
-                            <?php echo "<br>" ?>
-                        <?php endforeach; ?> -->
-                    </div>
-                </div>
-            </div>
-            
-            <div id="galeria">
-                <div class="estilo-img posicion-div-info">
-                    <div>
-                        <img src="<?= $body['peliculas']->ruta_cartel ?>" class="imgPerfilFichaIndividual" />
-                    <br/>
-                        <h4><?= $body['peliculas']->titulo ?></h4>
-                    </div>
-                    <div>
-                        <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#avisoCambio">Añadir fotos</button>
-                    </div>
-                </div>
-
-                <div class="posicion-div-info">
-                    <form action="<?= base_url()?>reparto/insertarImagenes" method="post" enctype="multipart/form-data">
-                        <div class="row" id="galeriaFotografica"></div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </section>
+		<div id="tabs" class="divPestanas">
+			<ul>
+				<li><a href="#principal"><i class="far fa-id-card"></i></a></li>
+				<li><a href="#sinopsis"> <i class="fas fa-book"></i> Sinopsis </a></li>
+				<li><a href="#reparto"> <i class="fas fa-film"></i> Reparto </a></li>
+				<!--<li><a href="#galeria"> <i class="far fa-images"></i> Galería </a></li>-->
+			</ul>
+			<div id="principal">
+				<div class="col-md-3" style="padding-top:20px;">
+    				<div class="rowd" style="float: left;display: inline-grid;height: 100%;margin: 1% 1% 5% 0;">
+    					<div class="col-md-12">
+    						<img src="<?= $body['peliculas']->ruta_cartel ?>" class="imgPerfilFichaIndividual" />
+    					</div>
+    					<div class="col-md-12">
+    						<h4><?= $body['peliculas']->titulo ?></h4>
+    					</div>
+    					<?php if(isset($_SESSION['idUser'])):?>
+                        <div class="info-gen aviso-user" title="Has calificado con <?=isset($body['votos']['voto'])?$body['votos']['voto']:'0'?>/5 esta película">
+							<input type="hidden" id="userId" value="<?=$_SESSION['idUser']?>" name="user"/>
+							<input name="rating" value="<?=isset($body['votos']['voto'])?$body['votos']['voto']:' '?>" id="rating_star" type="hidden" postID="<?=$body['peliculas']['id']?>" />
+                            </div>
+						<?php else: ?>
+                            <a class="info-gen" href="<?=base_url()?>Login/loginGet" title="Debes estar logueado para poder votar esta película">
+							<input type="hidden" id="userId" value="0" name="user"/>
+							<input name="rating" value="<?=$body['peliculas']['media_votos_totales']?>" id="rating_star" type="hidden" postID="<?=$body['peliculas']['id']?>" />
+                            </a>
+    					<?php endif; ?>
+    					<div class="overall-rating">
+						    <h6>(Calificación media <span id="avgrat"><?php echo $body['peliculas']['media_votos_totales']; ?>
+						    </span>
+                            <br/>basada en <span id="totalrat"><?php echo $body['peliculas']['votos_totales']; ?></span> votos totales)</h6>
+						</div>
+    				</div>
+				</div> 
+				<div class="col-md-9" style="padding-top:20px;">
+    				<div class="row-md-12" style="height: 100%;margin: 1% 1% 5% 0;">
+        				<h5>Título original: <?=$body['peliculas']->titulo_original ?></h5>
+        				<h5>Fecha de estreno: <?=$body['peliculas']->fecha_lanzamiento ?></h5>
+        				Géneros: 
+    					<?php foreach ($body['peliculas']->sharedGenerosList as $gen): ?>
+							 <?= $gen->nombre ?>
+						<?php endforeach; ?>
+						<h5>¿Todos los públicos? <?= $body['peliculas']->adulto ?></h5>
+        				<h5>Idioma original: <?= $body['peliculas']->original_language ?></h5>
+    				</div>
+				</div>
+			</div>
+			
+			<div id="sinopsis">
+				<div class="col-md-3" style="padding-top:20px;">
+    				<div class="rowd" style="float:left; display:inline-grid; height:100%; margin:1% 1% 5% 0;">
+    					<div class="col-md-12">
+    						<img src="<?= $body['peliculas']->ruta_cartel ?>" class="imgPerfilFichaIndividual" />
+    					</div>
+    					<div class="col-md-12">
+    						<h4><?= $body['peliculas']->titulo ?></h4>
+    					</div>
+    				</div>
+				</div>
+				<div class="col-md-9" style="padding-top:20px;">
+    				<div class="row-md-12" style="height: 100%;margin: 1% 1% 5% 0;">
+    					<?= $body['peliculas']->sinopsis ?>
+    				</div>
+				</div>
+			</div>
+			
+			<div id="reparto">
+				<div class="col-md-3" style="padding-top:20px;">
+    				<div class="rowd" style="float: left;display: inline-grid;height: 100%;margin: 1% 1% 5% 0;">
+    					<div class="col-md-12">
+    						<img src="<?= $body['peliculas']->ruta_cartel ?>" class="imgPerfilFichaIndividual" />
+    					</div>
+    					<div class="col-md-12">
+    						<h4><?= $body['peliculas']->titulo ?></h4>
+    					</div>
+    				</div>
+				</div>
+				<div class="col-md-9 datosDentro">
+    				<div class="col-md-12" style="height: 100%;margin: 1% 1% 5% 0;">
+    					<h4>Reparto de la película:</h4>
+	    				<?php foreach($body['peliculas']->sharedRepartosList as $dir): ?>
+    						<div class="col-md-6" id="<?= $dir->id ?>" onclick="mostrarFicha(this.id);">
+	                        	<img src="<?= $dir->ruta_foto ?>" class="imgPerfilFichaIndividual" />
+	                        	<?= $dir->nombre ?> <?= $dir->apellido1 ?> <?= $dir->apellido2 ?>
+                        	</div>
+	                    <?php endforeach; ?>
+    				</div>
+				</div>
+			</div>
+			
+			<!-- GALERÍa
+			<div id="galeria">
+				<div class="col-md-3" style="padding-top:20px;">
+    				<div class="rowd" style="float: left;display: inline-grid;height: 100%;margin: 1% 1% 5% 0;">
+    					<div class="col-md-12">
+    						<img src="<?= $body['peliculas']->ruta_cartel ?>" class="imgPerfilFichaIndividual" />
+    					</div>
+    					<div class="col-md-12">
+    						<h4><?= $body['peliculas']->titulo ?></h4>
+    					</div>
+    					<?php //if($body['usuarios']->rol == 3): ?>
+    					<div class="col-md-12" style="height: 100%;margin: 1% 1% 5% 0;">
+    						<input class="btn btn-default" type="button" onclick="insertarFotos();" value="Añadir fotos" />
+    					</div>
+    					<?php //endif; ?>
+    				</div>
+				</div>
+    			<div class="col-md-9" style="padding-top:20px;">
+    				<form action="<?= base_url()?>reparto/insertarImagenes" method="post" enctype="multipart/form-data">
+    					<div class="rowd" id="galeriaFotografica"></div>
+    				</form>
+				</div>
+			</div>-->
+		</div>
+	</section>
 </div>
 
     <div class="modal fade" id="avisoCambio" tabindex="-1" role="dialog" aria-labelledby="avisoCambioModalLabel" aria-hidden="true">
--- application/views/reparto/ficha.php
+++ application/views/reparto/ficha.php
@@ -1,15 +1,19 @@
-<?php if ( isset($_SESSION['rol']) && ($_SESSION['rol'] == "administrador")):?>
-<div class="content-wrapper">
-<?php else: ?>
+<script>
+    $(document).ready(function(){
+        $( '.info-gen' ).tooltip();
+
+        $("#tabs").tabs();
+    });
+</script>
+<?php if( !(isset ( $_SESSION ['rol'])) || ((isset ( $_SESSION ['rol'])) && ($_SESSION ['rol']!="administrador"))): ?>
 <div class="container content-wrapper">
-<?php endif;?>
 	<section class="content">
 		<div id="tabs" style="height:500px;">
 			<ul>
 				<li><a href="#perfil"><i class="far fa-id-card"></i></a></li>
 				<li><a href="#bio"> <i class="fas fa-book"></i> Biografía </a></li>
 				<li><a href="#filmog"> <i class="fas fa-film"></i> Filmografía </a></li>
-				<li><a href="#galeria"> <i class="far fa-images"></i> Galería </a></li>
+				<!--<li><a href="#galeria"> <i class="far fa-images"></i> Galería </a></li>-->
 			</ul>
 		
 			<div id="perfil"> 
@@ -81,7 +85,7 @@
     				</div>
 				</div>
 			</div>
-			
+			<!-- GALERÍA
 			<div id="galeria">
 				<div class="col-md-3" style="padding-top:20px;">
     				<div class="rowd datosReparto">
@@ -91,7 +95,6 @@
     					<div class="col-md-12">
     						<h4><?= $body['repartos']->nombre ?> <?= $body['repartos']->apellido1 ?> <?= $body['repartos']->apellido2 ?></h4>
     					</div>
-    					<!-- Cuando se pase el rol del usuario, si es admin, puede ver el botón, si no, solo las fotos -->
     					<?php //if($body['usuarios']->rol == 3): ?>
     					<div class="col-md-12" style="height: 100%;margin: 1% 1% 5% 0;">
     						<input class="btn btn-default" type="button" onclick="insertarFotos();" value="Añadir fotos" />
@@ -99,16 +102,161 @@
     					<?php //endif; ?>
     				</div>
 				</div>
-				<!-- TEMPORAL añadir imagenes a la galería de cada persona -->
     			<div class="col-md-9" style="padding-top:20px;">
     				<form action="<?= base_url()?>reparto/insertarImagenes" method="post" enctype="multipart/form-data">
     					<div class="rowd" id="galeriaFotografica"></div>
     				</form>
 				</div>
-			</div>
+			</div>-->
 		</div>
 	</section>
 </div>
+
+
+
+
+<!--
+
+
+                VISTA DEL ADMINISTRADOR DESDE AQUI HACIA ABAJO
+
+
+ -->
+<?php else: ?>
+<div class="content-wrapper">
+    <section class="content">
+		<div id="tabs" style="height:500px;">
+			<ul>
+				<li><a href="#perfil"><i class="far fa-id-card"></i></a></li>
+				<li><a href="#bio"> <i class="fas fa-book"></i> Biografía </a></li>
+				<li><a href="#filmog"> <i class="fas fa-film"></i> Filmografía </a></li>
+				<!--<li><a href="#galeria"> <i class="far fa-images"></i> Galería </a></li>-->
+			</ul>
+		
+			<div id="perfil"> 
+				<div class="col-md-3" style="padding-top:20px;">
+    				<div class="rowd datosReparto">
+    					<div class="col-md-12">
+    						<img src="<?= $body['repartos']->rutaFoto ?>" style="width:250px; height:300px;" class="imgPerfilFichaIndividual" />
+    					</div>
+    					<div class="col-md-12">
+    						<h4><?= $body['repartos']->nombre ?> <?= $body['repartos']->apellido1 ?> <?= $body['repartos']->apellido2 ?></h4>
+    					</div>
+    				</div>
+				</div>
+				<div class="col-md-9" style="float:left;padding-top:20px;">
+    				<div class="row-md-12 datosDentro">
+    					<h5>
+							<?php foreach($body['profesiones'] as $profesion): ?>
+    							<?= $profesion->nombre ?> ||
+    						<?php endforeach; ?>
+						</h5>
+        				<h5>Fecha de nacimiento: <?=$body['repartos']->fechaNacimiento ?></h5>
+        				<h5>País de nacimiento: <?= $body['repartos']->paises['nombre'] ?></h5>
+    				</div>
+				</div>
+			</div>
+			
+			<div id="bio">
+				<div class="col-md-3" style="padding-top:20px;">
+    				<div class="rowd datosReparto">
+    					<div class="col-md-12" >
+    						<img src="<?= $body['repartos']->rutaFoto ?>" style="width:250px; height:300px;" class="imgPerfilFichaIndividual" />
+    					</div>
+    					<div class="col-md-12">
+    						<h4><?= $body['repartos']->nombre ?> <?= $body['repartos']->apellido1 ?> <?= $body['repartos']->apellido2 ?></h4>
+    					</div>
+    				</div>
+				</div>
+    			<div class="col-md-9 datosDentro">
+    				<div class="row-md-12">
+    					<?= $body['repartos']->biografia ?>
+    				</div>
+				</div>
+			</div>
+			
+			<div id="filmog">
+				<div class="col-md-3" style="padding-top:20px;">
+					<div class="rowd datosReparto">
+    					<div class="col-md-12">
+    						<img src="<?= $body['repartos']->rutaFoto ?>" class="imgPerfilFichaIndividual" />
+    					</div>
+    					<div class="col-md-12">
+    						<h4><?= $body['repartos']->nombre ?> <?= $body['repartos']->apellido1 ?> <?= $body['repartos']->apellido2 ?></h4>
+    					</div>
+    				</div>
+				</div>
+				<div class="col-md-9 datosDentro">
+    				<div class="col-md-12">
+    					<h3>Películas en las que participa:</h3>
+    					<?php foreach ($body['repartos']->sharedPeliculasList as $pel): ?>
+							<div class="col-md-6" id="<?= $pel->id ?>"onclick="mostrarFicha(this.id);" style="margin-top:40px;">
+    							<div class="row-md-3" style="display:inline">
+	    							<img src="<?= $pel->ruta_cartel ?>" style="width:100px; height:150px;" />
+	    						</div>
+		    					<div class="col-md-8" style="display:inline">
+				    				<?= $pel->titulo ?> 
+		    					</div>
+    						</div>
+						<?php endforeach; ?>
+    				</div>
+				</div>
+			</div>
+			<!-- GALERÍA
+			<div id="galeria">
+				<div class="col-md-3" style="padding-top:20px;">
+    				<div class="rowd datosReparto">
+    					<div class="col-md-12">
+    						<img src="<?= $body['repartos']->rutaFoto ?>" style="width:250px; height:300px;" class="imgPerfilFichaIndividual" />
+    					</div>
+    					<div class="col-md-12">
+    						<h4><?= $body['repartos']->nombre ?> <?= $body['repartos']->apellido1 ?> <?= $body['repartos']->apellido2 ?></h4>
+    					</div>
+    					<?php //if($body['usuarios']->rol == 3): ?>
+    					<div class="col-md-12" style="height: 100%;margin: 1% 1% 5% 0;">
+    						<input class="btn btn-default" type="button" onclick="insertarFotos();" value="Añadir fotos" />
+    					</div>
+    					<?php //endif; ?>
+    				</div>
+				</div>
+    			<div class="col-md-9" style="padding-top:20px;">
+    				<form action="<?= base_url()?>reparto/insertarImagenes" method="post" enctype="multipart/form-data">
+    					<div class="rowd" id="galeriaFotografica"></div>
+    				</form>
+				</div>
+			</div>-->
+		</div>
+	</section>
+</div>
+
+    <div class="modal fade" id="avisoCambio" tabindex="-1" role="dialog" aria-labelledby="avisoCambioModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+            <h3 class="modal-title" id="avisoCambioModalLabel">Galería</h3>
+          </div>
+          <div class="modal-body">
+            <form enctype="multipart/form-data" action="uploader.php" method="POST">
+              <div class="form-group">
+                <label for="cast" class="col-form-label">Imagenes de: </label><br/><br/>
+                <input type="file" name="archivos" multiple="multiple">
+              </div>
+            <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" id="close" data-dismiss="modal">Cancelar</button>
+            <button type="submit" class="btn btn-info" id="open">Subir fotos</button>
+            </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+<?php endif; ?>
+
+
+
 
 <script type="text/javascript">
 	function insertarFotos() {
